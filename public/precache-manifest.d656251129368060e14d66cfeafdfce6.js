ba48bcface9727e8efaf0efeb7b7edc6bb4865a9
ArtemIvasenko <38789257+ArtemIvasenko@users.noreply.github.com>
2022-08-01 22:27:59 +0300

    dist

---
+++ public/precache-manifest.d656251129368060e14d66cfeafdfce6.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3dfcf08f1627c1bea2",
+    "url": "/a108/css/app.bd150d35.css"
+  },
+  {
+    "revision": "db2c75b2fb0f8e2ee9b671e904592643",
+    "url": "/a108/data0.json"
+  },
+  {
+    "revision": "858bd98638c6122b1bb68a18db9db22e",
+    "url": "/a108/data1.json"
+  },
+  {
+    "revision": "6f2d89e76422ee5650696cf2a98c3917",
+    "url": "/a108/data2.json"
+  },
+  {
+    "revision": "99115c361755568bfba598d734df6452",
+    "url": "/a108/data3.json"
+  },
+  {
+    "revision": "45730d91b30196cb6a34bd1fee4c1304",
+    "url": "/a108/img/changeSymbolBG.45730d91.jpg"
+  },
+  {
+    "revision": "29d056a85118a72e6a8eaa9a3da1a18d",
+    "url": "/a108/img/header-bg.29d056a8.png"
+  },
+  {
+    "revision": "0e83f638e008669327fca157b377fe2e",
+    "url": "/a108/img/icon-bg.0e83f638.png"
+  },
+  {
+    "revision": "5c498ef1809870eff80cd1ab2c9c7d2c",
+    "url": "/a108/img/icon-effect.5c498ef1.svg"
+  },
+  {
+    "revision": "b3b06e618535bb5b383441afb16210a1",
+    "url": "/a108/img/Близнецы.b3b06e61.svg"
+  },
+  {
+    "revision": "906420ca3f03c74306772ba8b3b9480b",
+    "url": "/a108/img/Весы.906420ca.svg"
+  },
+  {
+    "revision": "dec2a90cade4976a09d6142b36f682ac",
+    "url": "/a108/img/Водолей.dec2a90c.svg"
+  },
+  {
+    "revision": "efa7509c3fa0daee59e60ee9063cc8c6",
+    "url": "/a108/img/Дева.efa7509c.svg"
+  },
+  {
+    "revision": "f7350c0a67b9313e89c29f016f06c936",
+    "url": "/a108/img/Козерог.f7350c0a.svg"
+  },
+  {
+    "revision": "5a0bdc32fbce9c2d266b3da259388cfb",
+    "url": "/a108/img/Лев.5a0bdc32.svg"
+  },
+  {
+    "revision": "7c510b10839e30a9f293e3e11d50dc08",
+    "url": "/a108/img/Овен.7c510b10.svg"
+  },
+  {
+    "revision": "05fc93ad8ec587ca69ecb22853e6ea44",
+    "url": "/a108/img/РазборСудьбы.05fc93ad.jpg"
+  },
+  {
+    "revision": "68515cf72fd02f7738b879060cbebd37",
+    "url": "/a108/img/Рак.68515cf7.svg"
+  },
+  {
+    "revision": "b776f762fc1ea431e0a93ddf31da4b04",
+    "url": "/a108/img/Рыбы.b776f762.svg"
+  },
+  {
+    "revision": "dc78f017a1b28f1cef3c61f62ec9300a",
+    "url": "/a108/img/Скорпион.dc78f017.svg"
+  },
+  {
+    "revision": "ecf41c88e4de42d10f22868d916b0c26",
+    "url": "/a108/img/Стрелец.ecf41c88.svg"
+  },
+  {
+    "revision": "80c803a1fea954709c9236da831d103e",
+    "url": "/a108/img/Телец.80c803a1.svg"
+  },
+  {
+    "revision": "8b4fc91b3e59f5c40539fedab7d10c19",
+    "url": "/a108/index.html"
+  },
+  {
+    "revision": "8bf575b6462109d4a276",
+    "url": "/a108/js/about.1ada8f18.js"
+  },
+  {
+    "revision": "4c3dfcf08f1627c1bea2",
+    "url": "/a108/js/app.4114ae9d.js"
+  },
+  {
+    "revision": "f506e5d2621001ae9013",
+    "url": "/a108/js/chunk-vendors.9a2ce12c.js"
+  },
+  {
+    "revision": "cf5b0cd4394f4617cb8d0dcbe6114b47",
+    "url": "/a108/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/a108/robots.txt"
+  }
+]);
